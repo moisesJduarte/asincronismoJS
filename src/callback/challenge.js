2aa8875045138d0f40cb98dbecacf75dc6954936
moisesJduarte <gladmd19@gmail.com>
2022-05-09 22:22:58 -0400

    Xhttp y comentarios

---
+++ src/callback/challenge.js
@@ -0,0 +1,34 @@
+let XMLhttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+function fetchData(url_api, callback) {
+    let xhttp = new XMLhttpRequest();
+    //Los estados de un request de acuerdo a la documentacion:
+//0: request not initialized
+//1: server connection established
+//2: request received
+//3: processing request
+//4: request finished and response is ready
+
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = function (event) {
+        if(xhttp.readyState === 4) {
+            if(xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error('Error ' + url_api);
+                return callback(error, null)
+            }
+        }
+    }
+    xhttp.send();
+}
+//1porque no voy usar fetch : esta disponible en es6 en adelate es paera hacer peticiones y trabaja con promesas
+//Aclaraciòn de varias cosas que quizás no entiendas si estas empezando:
+
+//2.A)XMLHttpRequest es la forma antigua de hacer llamados, como el profesor lo menciona usa ese y no Fetch que es el actual, por que no conocemos aùn las promesas y fecth es con promesas, para saber por que el profesor uso OPEN y todas esas funciones aqui està la forma de usar XMLHttpRequest : https://developer.mozilla.org/es/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest.
+
+//B)" new Error " que el profesor crea, es una nueva instancia de la clase Error que tiene Javascript, son clases ya implicitas que tiene javascript en este caso es para monstrar bien un mensaje de error podemos usarla, màs informaciòn aqui : https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Error.
+
+//C) los que son fron-end y están aprendiendo de Back, el profesor uso GET por que hace parte de los método http, en este caso necesitamos pedir información a las url ,màs información: https://developer.mozilla.org/es/docs/Web/HTTP/Methods
+
+//D)Por ultimo recomiendo una escucha atenta a lo que dice el profesor por que el explica el por que de cada cosa que hace y si no la conoces recomiendo buscarlas en Internet y asì avanzas en el curso.
